c405b081497581cace649255cc55de3689f91ee5
HuaMick <Hua.Mick@gmail.com>
2022-08-29 22:43:59 +1000

    20220829

---
+++ view.py
@@ -0,0 +1,5 @@
+
+#Want to figure out how to use this with streamlit + docker
+#import debugpy
+#debugpy.listen(('0.0.0.0', 777))
+#debugpy.wait_for_client()  # blocks execution until client is attached
